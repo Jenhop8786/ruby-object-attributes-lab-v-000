ea62bffbf38de068aab91035adbabbfff1ad45cc
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-11-17 16:32:24 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+fido =Dog.new
+fido.name = "fido"
+fido.breed = "beagle"
+
+class Dog
+  def name=(fido)
+    @name= fido
+
+def name
+  @name
+
+  def breed=(beagle)
+    @breed= beagle
+
+    def breed
+      @breed
+    end
+    end
+  end
+end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,23 @@
+class Person
+ def name=(Beyonce)
+ @name = Beyonce
+
+ def name
+   @name
+   puts "Beyonce"
+
+   def job=(Singer)
+     @job = singer
+
+     def job
+       @job
+       puts "Singer"
+     end
+   end
+ end
+end
+ end
+
+ Beyonce = Person.new
+ Beyonce.name = Beyonce
+ Beyonce.job = Singer 
